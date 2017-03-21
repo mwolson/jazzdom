081f2b3798773b9419f35ebe5e8e49664d2785bb
Mike Olson <mwolson@member.fsf.org>
2017-03-20 22:48:12 -0700

    Initial contents

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import jsdom from 'jsdom'
+import jsdomGlobal from 'jsdom-global'
+
+const defaultDestroyFn = () => {}
+let destroyFn = defaultDestroyFn
+
+export const destroy = () => Promise.resolve().then(() => {
+  destroyFn()
+  destroyFn = defaultDestroyFn
+})
+
+export function create(html, { virtualConsole, created, ...restOptions }={}) {
+  return Promise.resolve().then(() => {
+    return destroyFn !== defaultDestroyFn ? destroy() : Promise.resolve()
+  })
+  .then(() => new Promise((resolve, reject) => {
+    destroyFn = jsdomGlobal(html, {
+      virtualConsole: virtualConsole || jsdom.createVirtualConsole().sendTo(console),
+      created: (err, window) => {
+        err ? reject(err) : resolve(window)
+        created && created(err, window)
+      },
+      ...restOptions,
+    })
+  }))
+}
