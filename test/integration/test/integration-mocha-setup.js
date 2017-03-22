3ad0835f36c87cdbb6e66528177e1a1413d0c113
Mike Olson <mwolson@member.fsf.org>
2017-03-22 14:51:11 -0700

    Fix build error on node v4

--- test/integration/test/integration-mocha-setup.js
+++ test/integration/test/integration-mocha-setup.js
@@ -14,14 +14,3 @@ global.expect = chai.expect
 process.on('unhandledRejection', function(err) {
   throw err
 })
-
-// if there are any PropType warnings from React, treat them as fatal errors
-const realLogger = console.error
-console.error = function error(...args) {
-  const msg = args.join(' ')
-  if (~msg.indexOf('Failed prop type')) {
-    throw new Error(msg)
-  } else {
-    return realLogger(...args)
-  }
-}
